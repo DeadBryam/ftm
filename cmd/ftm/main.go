08d64b528c3bf8f2e12b79cfbac8997b7685f3a8
Bryan Villafuerte <bryamu.u@gmail.com>
2026-06-03 22:24:27 -0600

    refactor(cli): extract update command to internal/cli
    Update(checkOnly bool) returns error. Removes updater dep
    from main and moves all i18n/update logic out of main.go.

--- cmd/ftm/main.go
+++ cmd/ftm/main.go
@@ -8,42 +8,10 @@ import (
 	"github.com/sthbryan/ftm/internal/app"
 	"github.com/sthbryan/ftm/internal/cli"
 	"github.com/sthbryan/ftm/internal/i18n"
-	"github.com/sthbryan/ftm/internal/updater"
 	"github.com/sthbryan/ftm/internal/version"
 )
 
 var BuildVersion string
-
-func doUpdate(checkOnly bool) {
-	_ = i18n.Load()
-	u := updater.New("sthbryan/ftm")
-	info, err := u.Check(version.Version)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, i18n.TF("update_check_failed", err.Error())+"\n")
-		os.Exit(1)
-	}
-
-	fmt.Println(i18n.TF("update_current_version", info.CurrentVersion))
-	fmt.Println(i18n.TF("update_latest_version", info.LatestVersion))
-
-	if !info.HasUpdate {
-		fmt.Println(i18n.T("update_up_to_date"))
-		return
-	}
-
-	fmt.Println(i18n.TF("update_available", info.Tag))
-	fmt.Println(i18n.TF("update_release_url", info.ReleaseURL))
-	if checkOnly {
-		return
-	}
-
-	fmt.Println(i18n.TF("update_downloading", info.AssetName))
-	if err := u.Apply(info); err != nil {
-		fmt.Fprintf(os.Stderr, i18n.TF("update_apply_failed", err.Error())+"\n")
-		os.Exit(1)
-	}
-	fmt.Println(i18n.TF("update_success", info.LatestVersion))
-}
 
 func main() {
 	_ = cli.Init()
@@ -72,12 +40,18 @@ func main() {
 	}
 
 	if *checkOnly {
-		doUpdate(true)
+		if err := cli.Update(true); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 		os.Exit(0)
 	}
 
 	if *update {
-		doUpdate(false)
+		if err := cli.Update(false); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
 		os.Exit(0)
 	}
 
---
+++ internal/cli/update.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/sthbryan/ftm/internal/i18n"
+	"github.com/sthbryan/ftm/internal/updater"
+	"github.com/sthbryan/ftm/internal/version"
+)
+
+const defaultRepo = "sthbryan/ftm"
+
+func Update(checkOnly bool) error {
+	u := updater.New(defaultRepo)
+	info, err := u.Check(version.Version)
+	if err != nil {
+		return fmt.Errorf("%s", i18n.TF("update_check_failed", err.Error()))
+	}
+
+	fmt.Println(i18n.TF("update_current_version", info.CurrentVersion))
+	fmt.Println(i18n.TF("update_latest_version", info.LatestVersion))
+
+	if !info.HasUpdate {
+		fmt.Println(i18n.T("update_up_to_date"))
+		return nil
+	}
+
+	fmt.Println(i18n.TF("update_available", info.Tag))
+	fmt.Println(i18n.TF("update_release_url", info.ReleaseURL))
+	if checkOnly {
+		return nil
+	}
+
+	fmt.Println(i18n.TF("update_downloading", info.AssetName))
+	if err := u.Apply(info); err != nil {
+		return fmt.Errorf("%s", i18n.TF("update_apply_failed", err.Error()))
+	}
+	fmt.Println(i18n.TF("update_success", info.LatestVersion))
+	return nil
+}
